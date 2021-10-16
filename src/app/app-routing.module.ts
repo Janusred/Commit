a0fc664fc226340f11eaf10184746e8cffa19773
Jansred <horaciok300@hotmail.com>
2021-10-16 02:01:49 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent} from 'src/app/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },{
+    path: 'index',
+    component: IndexComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
